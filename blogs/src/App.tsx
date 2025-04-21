8224713c2047a85e07bfb66312d9b29a872574a5
TeaChareqishvili <teachareqishvili89@gmail.com>
2025-04-21 14:40:54 +0400

    initial commit

---
+++ blogs/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black  w-[80%] h-[300px] text-[34px]">
+      <div className="bg-blue-500 text-white p-4">Hello, Tailwind!</div>
+    </div>
+  );
+}
+
+export default App;
