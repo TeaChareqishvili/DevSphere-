e329919ce59b459355e3f80db9d4ab8d77ec86b8
TeaChareqishvili <teachareqishvili89@gmail.com>
2025-04-26 19:13:41 +0400

    underline active link

--- blogs/src/components/Navigation.tsx
+++ blogs/src/components/Navigation.tsx
@@ -2,22 +2,24 @@ import { NavLink } from "react-router-dom";
 import { useContext } from "react";
 import { ThemeContext } from "../context/Theme/ThemeProvider";
 
-//TODO underline active link
-
 export default function Navigation() {
   const { theme } = useContext(ThemeContext);
   return (
     <>
       <nav className="navigation-nav">
         <NavLink
-          style={{ color: theme === "light" ? "#10474f" : "#ffffff" }}
+          style={({ isActive }) => ({
+            color: isActive ? "red" : theme === "light" ? "#10474f" : "#ffffff",
+          })}
           className="navigation-list"
           to="/"
         >
           All Blogs
         </NavLink>
         <NavLink
-          style={{ color: theme === "light" ? "#10474f" : "#ffffff" }}
+          style={({ isActive }) => ({
+            color: isActive ? "red" : theme === "light" ? "#10474f" : "#ffffff",
+          })}
           className="navigation-list"
           to="/favorite"
         >
