ca676dc061aa2d24c1f7427ec4721459e5aaf65c
TeaChareqishvili <teachareqishvili89@gmail.com>
2025-04-27 14:12:10 +0400

    get user session on profile page

--- blogs/src/components/Profile.tsx
+++ blogs/src/components/Profile.tsx
@@ -1,3 +1,23 @@
+import { useUser } from "@clerk/clerk-react";
+import Loader from "./Loader";
+
 export default function Profile() {
-  return <div>Profile page</div>;
+  const { user } = useUser();
+
+  console.log(user, "user");
+
+  if (!user) {
+    return <Loader />;
+  }
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Profile</h2>
+      <p>
+        <strong>Email:</strong> {user.emailAddresses[0].emailAddress}
+      </p>
+      <p>
+        <strong>Username:</strong> {user.username}
+      </p>
+    </div>
+  );
 }
